3b5242b6be008086bfce9d0f9aa3d007bb0433e9
Deniel Saskin <96053994+DeenkaS@users.noreply.github.com>
2022-09-09 17:34:49 +0200

    fixed testprogram

--- Sortingtest.java
+++ Sortingtest.java
@@ -9,6 +9,10 @@ public class Sortingtest {
             testarray[i] = rnd.nextInt(100); 
         }
 
+        for(int k = 0; k < testarray.length; k++){
+            System.out.println(testarray[k]);
+        }
+
         Sort.simple_sort(testarray);
 
         for(int j = 0; j < testarray.length; j++){
