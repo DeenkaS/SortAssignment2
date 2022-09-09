7a94c71ab142d1b3a59eca99ae58f01fbafc483e
Deniel Saskin <96053994+DeenkaS@users.noreply.github.com>
2022-09-09 16:42:45 +0200

    adding Benchmarking tool

---
+++ Bench.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Bench {
+    public static void main(String []args){
+        int k = 100_000;
+        int[] bencharray = {100,200,400,800,1600,3200,6400,12800};
+        Random rnd = new Random();
+        long t0 = 0;
+        long tsum = 0;
+
+
+        for(int n : bencharray){
+            
+            int[] array1 = new int[rnd.nextInt(n)];
+
+            for(int i = 0; i < k; i++){
+            t0 = System.nanoTime();
+            Sort.simple_sort(array1);
+            tsum += t0 - System.nanoTime();
+            }
+
+            System.out.println("Length of array: " + n + " average runtime: " + (tsum/k));
+
+
+
+        }
+    }
+    
+}
