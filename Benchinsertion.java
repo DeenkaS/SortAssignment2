e456cb0d4ad25256dee0c3be57c962f93921586a
Deniel Saskin <96053994+DeenkaS@users.noreply.github.com>
2022-09-13 22:08:36 +0200

    adding insertion Sort

---
+++ Benchinsertion.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Benchinsertion {
+    public static void main(String[] args) {
+        int k = 1000;
+        int[] bencharray = { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200,1300,1400,1500,1600,3200,6400 };
+        Random rnd = new Random();
+        long t0 = 0;
+        long tsum = 0;
+
+        for (int n : bencharray) {
+
+            int[] array1 = new int[rnd.nextInt(n)];
+
+            for (int i = 0; i < k; i++) {
+                t0 = System.nanoTime();
+                Sort.insertion_sort(array1);
+                tsum += System.nanoTime() - t0;
+            }
+
+            System.out.println("Length of array: " + n + " average runtime: " + (tsum / k));
+
+        }
+    }
+
+}
